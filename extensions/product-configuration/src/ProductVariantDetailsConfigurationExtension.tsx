6cd4305e1f22ff997ece0fd2d12a755feea92306
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-06-20 18:57:37 -0600

    product details warpify

--- extensions/product-configuration/src/ProductVariantDetailsConfigurationExtension.tsx
+++ extensions/product-configuration/src/ProductVariantDetailsConfigurationExtension.tsx
@@ -2,12 +2,14 @@ import {
   reactExtension,
   useApi,
   Text,
+  AdminAction,
+  Button,
 } from "@shopify/ui-extensions-react/admin";
 
 // The target used here must match the target used in the extension's toml file (./shopify.extension.toml)
 
 export default reactExtension<any>(
-  "admin.product-variant-details.configuration.render",
+  "admin.product-details.action.render",
   () => <App />
 );
 
@@ -17,5 +19,10 @@ function App() {
     i18n,
   } = useApi<"admin.product-variant-details.configuration.render">();
 
-  return <Text>"welcome degen mint your nft"</Text>;
+  return (
+    <AdminAction>
+      <Text>"welcome degen mint your nft"</Text>
+      <Button>Post on Warpcast</Button>
+    </AdminAction>
+  );
 }
